6c65da866c62590950e15cd86a2e35312b039a5d
RedSulfur <serhiizem@gmail.com>
2016-11-09 16:07:36 +0200

    Resizable array stack

---
+++ Topics/data-structures/princeton/src/test/java/princeton/week2/ResizableArrayStackTest.java
@@ -0,0 +1,49 @@
+package princeton.week2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ResizableArrayStackTest {
+
+    private ResizableArrayStack<String> arrayStack;
+
+    @Before
+    public void setUp() throws Exception {
+        arrayStack = new ResizableArrayStack<>();
+    }
+
+    @Test
+    public void shouldPopElementOnNonEmptyStack() {
+        arrayStack.push("John");
+        arrayStack.push("Doe");
+
+        assertThat(arrayStack.pop(), is("Doe"));
+        assertThat(arrayStack.pop(), is("John"));
+    }
+
+    @Test
+    public void shouldReturnCorrectSizeOfTheStack() {
+        arrayStack.push("John");
+        arrayStack.push("Doe");
+
+        assertThat(arrayStack.size(), is(2));
+    }
+
+    @Test
+    public void iteratorShouldIterateElementsOfTheStack() {
+        arrayStack.push("John");
+        Iterator<String> stackIterator = arrayStack.iterator();
+        String elementInStack = null;
+        while (stackIterator.hasNext()) {
+            elementInStack = stackIterator.next();
+        }
+        assertThat(elementInStack, is("John"));
+    }
+
+
+}
