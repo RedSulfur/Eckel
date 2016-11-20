fc79081fe9a1fb16ad76a24da78ae675e34d2544
RedSulfur <serhiizem@gmail.com>
2016-11-20 11:05:00 +0200

    Brute collinear points

--- Topics/data-structures/princeton/src/main/java/princeton/assignment3/BruteCollinearPoints.java
+++ Topics/data-structures/princeton/src/main/java/princeton/assignment3/BruteCollinearPoints.java
@@ -1,5 +1,9 @@
 package princeton.assignment3;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+
+import javax.sound.sampled.Line;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,65 +21,72 @@ import static java.nio.file.Paths.get;
 
 public class BruteCollinearPoints {
 
-    private static final BiFunction<Point, Point, Double> SLOPE_BETWEEN_POINTS_BIFUNCTION = Point::slopeTo;
+    private final static int MAX_SCALE_VALUE = 32768;
+    /**
+     * Constant holding radius for points drawing
+     */
+    private static final double POINT_RADIUS = 0.02;
+    /**
+     * Constant holding radius for lines drawing
+     */
+    private static final double LINE_RADIUS = 0.005;
 
     private static Point[] points;
     private static LineSegment[] lineSegments;
 
     public static void main(String[] args) throws IOException {
-        populatePointsArray();
-        new BruteCollinearPoints();
+        StdDraw.setXscale(0, MAX_SCALE_VALUE);
+        StdDraw.setYscale(0, MAX_SCALE_VALUE);
+        StdDraw.setPenRadius(POINT_RADIUS);
+        In input = new In(args[0]);
+        int n = input.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; !input.isEmpty(); i++) {
+            int x = input.readInt();
+            int y = input.readInt();
+            Point point = new Point(x, y);
+            point.draw();
+            points[i] = point;
+        }
+
+        StdDraw.setPenRadius(LINE_RADIUS);
+
+        BruteCollinearPoints bruteCollinearPoints = new BruteCollinearPoints(points);
     }
 
-    private static void populatePointsArray() throws IOException {
-        Pattern p = Pattern.compile("\\d+");
-
-        Stream<String> stream = lines(get("/home/serhii/old-system/" +
-                "old-system/IdeaProjects/Eckel/Topics/data-structures/princeton/" +
-                "src/main/java/princeton/assignment3/input400.txt"));
-        List<String> strings = stream.skip(1).collect(Collectors.toList());
-//        strings.forEach(System.out::println);
-        List<Point> pointsList = strings.stream().map(s -> {
-            Matcher m = p.matcher(s);
-            List<String> list = new ArrayList<>();
-            while (m.find()) {
-                list.add(m.group(0));
-            }
-            return new Point(Integer.valueOf(list.get(0)), Integer.valueOf(list.get(1)));
-        }).collect(Collectors.toList());
-
-        points = pointsList.toArray(new Point[pointsList.size()]);
-    }
-
-    public BruteCollinearPoints(/*Point[] points*/)  {
+    public BruteCollinearPoints(Point[] points)  {
 //        this.points = points;
 
         List<LineSegment> listOfLineSegments = new ArrayList<>();
 
         Arrays.sort(points);
 
-        for (int i = 0; i < points.length; i++) { //TODO: what is the upper bound
+        for (int i = 0; i < points.length - 3; i++) { //TODO: what is the upper bound
             Point p = points[i];
             for (int j = i + 1; j < points.length; j++) {
-                double slopePQ = p.slopeTo(points[j]);
+                Point q = points[j];
+                double slopePQ = p.slopeTo(q);
                 for (int k = j + 1; k < points.length; k++) {
+                    Point r = points[k];
+                    double slopePR = p.slopeTo(r);
+                    if (slopePQ != slopePR) continue;
                     for (int l = k + 1; l < points.length; l++) {
-
+                        Point s = points[l];
+                        double slopePS = p.slopeTo(s);
+                        if (slopePQ == slopePR && slopePQ == slopePS) {
+                            addLineSegment(listOfLineSegments, p, q, r, s);
+                            p.drawTo(s);
+                        }
                     }
                 }
             }
-
-
-            addLineSegments(listOfLineSegments, points[i],
-                    points[i + 1], points[i + 2], points[i + 3]);
         }
 
-        lineSegments = listOfLineSegments.toArray(new LineSegment[listOfLineSegments.size()]);
-        System.out.println("Constructed line segments");
+        System.out.println("Line segments:");
         listOfLineSegments.forEach(System.out::println);
     }
 
-    private void addLineSegments(List<LineSegment> lineSegments, Point... points) {
+    private void addLineSegment(List<LineSegment> lineSegments, Point... points) {
         System.out.println("points[0]" + points[0]);
         System.out.println("points[1]" + points[1]);
         System.out.println("points[2]" + points[2]);
@@ -83,28 +94,10 @@ public class BruteCollinearPoints {
         System.out.println("points[0].slopeTo(points[1])" + points[0].slopeTo(points[1]));
         System.out.println("points[0].slopeTo(points[2])" + points[0].slopeTo(points[2]));
         System.out.println("points[0].slopeTo(points[3])" + points[0].slopeTo(points[3]));
-        if(isSlopeBetweenPointsEqual(points[0], points[1], points[2])
-                && isSlopeBetweenPointsEqual(points[0], points[1], points[3])) {
 
-//            System.out.println("points[0]" + points[0]);
-//            System.out.println("points[1]" + points[1]);
-//            System.out.println("points[2]" + points[2]);
-//            System.out.println("points[3]" + points[3]);
-//            System.out.println("points[0].slopeTo(points[1])" + points[0].slopeTo(points[1]));
-//            System.out.println("points[0].slopeTo(points[2])" + points[0].slopeTo(points[2]));
-//            System.out.println("points[0].slopeTo(points[3])" + points[0].slopeTo(points[3]));
-            lineSegments.add(new LineSegment(points[0], points[1]));
-            lineSegments.add(new LineSegment(points[1], points[2]));
-            lineSegments.add(new LineSegment(points[2], points[3]));
-        }
-    }
-
-    private boolean isSlopeBetweenPointsEqual(Point p1, Point p2, Point p3) {
-        return getSlope(p1, p2) == getSlope(p1, p3);
-    }
-
-    private double getSlope(Point p1, Point p2) {
-        return SLOPE_BETWEEN_POINTS_BIFUNCTION.apply(p1, p2);
+        lineSegments.add(new LineSegment(points[0], points[1]));
+        lineSegments.add(new LineSegment(points[1], points[2]));
+        lineSegments.add(new LineSegment(points[2], points[3]));
     }
 
     public int numberOfSegments() {
